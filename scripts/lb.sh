e502739bf01b4fd0eeec25892d0bad3a7fb5688f
Random._. <rishangbhavsarcs@gmail.com>
2025-07-09 10:43:05 +0000

    fix: ip

--- scripts/lb.sh
+++ scripts/lb.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
-set -o errexit
+set -xo errexit
 
 # https://kind.sigs.k8s.io/docs/user/loadbalancer/
 
-ip=`docker network inspect -f '{{.IPAM.Config}}' kind | awk '{print $1}' | grep -oE "[0-9./]+" | cut -d '/' -f1`
+# Get the kind network IPv4 subnet
+ip=$(docker network inspect kind | jq -r '.[0].IPAM.Config[] | select(.Subnet | test("^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+")) | .Subnet' | cut -d'/' -f1)
+echo "ip: ${ip}"
 
 metallb_version="${METALLB_VERSION:=v0.13.7}"
 echo -e "${!metallb_version@}: ${metallb_version}\n"
@@ -21,11 +23,12 @@ metadata:
   namespace: metallb-system
 spec:
   addresses:
-  - ${ip}/24
+  - "${ip}/24"
 ---
 apiVersion: metallb.io/v1beta1
 kind: L2Advertisement
 metadata:
   name: empty
   namespace: metallb-system
+spec: {}
 EOF
